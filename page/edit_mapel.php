cb4bfaf9ce289e15d79a57a902e999c27164d590
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2026-04-01 20:54:09 +0700

    index.php

---
+++ page/edit_mapel.php
@@ -0,0 +1,61 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Edit Mata Pelajaran</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+$Kd = $_GET['Kd'];
+$edit = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM mapel WHERE Kd_mapel='$Kd'"));
+
+if(isset($_POST['tambah'])){
+    $Kd_mapel = $_POST['Kd_mapel'];
+    $Nm_mapel = $_POST['Nm_mapel'];
+    $Kkm = $_POST['Kkm'];
+
+    $insert = mysqli_query($koneksi, "UPDATE mapel SET Nm_mapel='$Nm_mapel', Kkm='$Kkm' WHERE Kd_mapel='$Kd_mapel'");
+    if($insert){
+        echo '<div class="alert alert-info-dismissible">
+        <button type="button" class="close" data-dismiss="alert"aria-hidden="true">X</button>
+        <h5><i class="icon fas fa-info"></i> Info </h5>
+        <h4>Berhasil Disimpan</h4></div>';
+        echo '<meta http-equiv="refresh" content="1;url=index.php?page=mapel">';
+    }else {
+       echo '<div class="alert alert-warning-dismissible">
+       <button type="button" class="close" data-dismiss="alert"aria-hidden="true">X</button>
+       <h5><i class="icon fas fa-info"></i> Info </h5>
+       <h4>Gagal Disimpan</h4></div>';
+    }
+}
+?>
+<section class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-body p-2">
+                    <form method="POST" action="">
+                        <div class="form-group">
+                            <label for="Kd_mapel">Kode Mapel</label>
+                            <input type="text" name="Kd_mapel" value="<?=$edit['Kd_mapel']; ?>" class="form-control" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="Nm_mapel">Nama Mapel</label>
+                            <input type="text" name="Nm_mapel" value="<?=$edit['Nm_mapel']; ?>" id="Nm_mapel" placeholder="Nama Mapel" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="Kkm">KKM</label>
+                            <input type="text" name="Kkm" value="<?=$edit['Kkm']; ?>" id="Kkm" placeholder="KKM" class="form-control">
+                        </div>
+                        <div class="card-footer">
+                            <input type="submit" class="btn btn-primary" name="tambah" value="simpan">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- page/tambah_mapel.php
+++ page/tambah_mapel.php
@@ -46,7 +46,7 @@
                     <div class="card-body p-2">
                         <form method="POST" action="">
                             <div class="form-group">
-                                <label>Kode Mapel</label>
+                                <label for="Kd_mapel">Kode Mapel</label>
                                 <input type="text" name="Kd_mapel" value="<?=$hasilkode; ?>" placeholder="Id Kat" class="form-control" readonly>
                             </div>
                             <div class="form-group">
